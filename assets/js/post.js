80a8cc449dd33161e8fce909447a414f4f0be689
gurwinderintel <gurwinder.singh@intel.com>
2026-03-28 06:30:38 -0700

    Set Disqus shortname, fix TOC overlap with suggested posts

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,15 +1,35 @@
-// TOC active-link scroll spy
+// TOC active-link scroll spy + hide when past article
 (function () {
+    var tocEl   = document.getElementById('toc-sidebar');
     var tocLinks = document.querySelectorAll('.toc-nav a');
     if (!tocLinks.length) return;
+
+    // Sentinel: bottom of the article post div
+    var articlePost = document.querySelector('.article-post');
+
     var headings = [];
     tocLinks.forEach(function (a) {
         var id = decodeURIComponent(a.getAttribute('href').replace('#', ''));
         var el = document.getElementById(id);
         if (el) headings.push({ el: el, a: a });
     });
+
     function onScroll() {
         var scrollY = window.scrollY + 100;
+
+        // Hide TOC once the user scrolls past the article content
+        if (tocEl && articlePost) {
+            var articleBottom = articlePost.getBoundingClientRect().bottom + window.scrollY;
+            if (scrollY > articleBottom) {
+                tocEl.style.opacity = '0';
+                tocEl.style.pointerEvents = 'none';
+            } else {
+                tocEl.style.opacity = '1';
+                tocEl.style.pointerEvents = 'auto';
+            }
+        }
+
+        // Active link highlight
         var active = null;
         for (var i = 0; i < headings.length; i++) {
             if (headings[i].el.offsetTop <= scrollY) active = headings[i];
